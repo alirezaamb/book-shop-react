171ca4aab91a19374cccf418897e8c6a5c548869
alirezaamb <ababaei78@gmail.com>
2024-05-23 16:30:32 +0330

    feat:create the header

---
+++ src/constants/localization.ts
@@ -0,0 +1,11 @@
+export const localization = {
+  home: 'خانه',
+  products: 'محصولات',
+  aboutUs: 'درباره ما',
+};
+
+export const pageLevelLocalization = {
+  homePage: {},
+  aboutUs: {},
+  products: {},
+};
