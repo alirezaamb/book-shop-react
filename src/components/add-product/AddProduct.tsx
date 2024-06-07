27ec9548b571728d268c6a509e824bcd9dac5970
alirezaamb <ababaei78@gmail.com>
2024-06-07 10:50:09 +0330

    fix:the style of form

--- src/components/add-product/AddProduct.tsx
+++ src/components/add-product/AddProduct.tsx
@@ -6,9 +6,9 @@ import {
   Grid,
   InputLabel,
   TextField,
-} from "@mui/material";
-import { pageLevelLocalization } from "../../constants/localization";
-import { useForm, SubmitHandler } from "react-hook-form";
+} from '@mui/material';
+import { pageLevelLocalization } from '../../constants/localization';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 type Inputs = {
   name: string;
@@ -30,65 +30,89 @@ export default function AddProduct() {
   };
 
   return (
-    <Box sx={{ direction: "rtl" }}>
+    <Box sx={{ direction: 'rtl' }}>
       <Card>
-        <CardContent>
+        <CardContent sx={{ width: '80%' }}>
           <form onSubmit={handleSubmit(onsubmit)}>
-            <Grid container>
+            <Grid container spacing={3}>
               <Grid item xs={12}>
-                <InputLabel htmlFor={pageLevelLocalization.addProduct.name}>
+                <InputLabel
+                  sx={{ fontSize: '16px', fontWeight: '600' }}
+                  htmlFor={pageLevelLocalization.addProduct.name}
+                >
                   {pageLevelLocalization.addProduct.name}
                 </InputLabel>
                 <TextField
+                  fullWidth
                   id={pageLevelLocalization.addProduct.name}
                   aria-describedby={pageLevelLocalization.addProduct.name}
-                  {...register("name")}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <InputLabel htmlFor={pageLevelLocalization.addProduct.author}>
-                  {pageLevelLocalization.addProduct.author}
-                </InputLabel>
-                <TextField
-                  id={pageLevelLocalization.addProduct.author}
-                  aria-describedby={pageLevelLocalization.addProduct.author}
-                  {...register("author")}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <InputLabel htmlFor={pageLevelLocalization.addProduct.desc}>
-                  {pageLevelLocalization.addProduct.desc}
-                </InputLabel>
-                <TextField
-                  id={pageLevelLocalization.addProduct.desc}
-                  aria-describedby={pageLevelLocalization.addProduct.desc}
-                  {...register("desc")}
+                  {...register('name')}
                 />
               </Grid>
               <Grid item xs={12}>
                 <InputLabel
+                  sx={{ fontSize: '16px', fontWeight: '600' }}
+                  htmlFor={pageLevelLocalization.addProduct.author}
+                >
+                  {pageLevelLocalization.addProduct.author}
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  id={pageLevelLocalization.addProduct.author}
+                  aria-describedby={pageLevelLocalization.addProduct.author}
+                  {...register('author')}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel
+                  sx={{ fontSize: '16px', fontWeight: '600' }}
+                  htmlFor={pageLevelLocalization.addProduct.desc}
+                >
+                  {pageLevelLocalization.addProduct.desc}
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  id={pageLevelLocalization.addProduct.desc}
+                  aria-describedby={pageLevelLocalization.addProduct.desc}
+                  {...register('desc')}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel
+                  sx={{ fontSize: '16px', fontWeight: '600' }}
                   htmlFor={pageLevelLocalization.addProduct.translator}
                 >
                   {pageLevelLocalization.addProduct.translator}
                 </InputLabel>
                 <TextField
+                  fullWidth
                   id={pageLevelLocalization.addProduct.translator}
                   aria-describedby={pageLevelLocalization.addProduct.translator}
-                  {...register("translator")}
+                  {...register('translator')}
                 />
               </Grid>
               <Grid item xs={12}>
-                <InputLabel htmlFor={pageLevelLocalization.addProduct.price}>
+                <InputLabel
+                  sx={{ fontSize: '16px', fontWeight: '600' }}
+                  htmlFor={pageLevelLocalization.addProduct.price}
+                >
                   {pageLevelLocalization.addProduct.price}
                 </InputLabel>
                 <TextField
+                  fullWidth
                   id={pageLevelLocalization.addProduct.price}
                   aria-describedby={pageLevelLocalization.addProduct.price}
-                  {...register("price")}
+                  {...register('price')}
                 />
               </Grid>
-              <Grid item xs={12}>
-                <Button type="submit">ثبت</Button>
+              <Grid
+                item
+                xs={12}
+                sx={{ display: 'flex', justifyContent: 'end' }}
+              >
+                <Button variant="contained" type="submit">
+                  ثبت
+                </Button>
               </Grid>
             </Grid>
           </form>
